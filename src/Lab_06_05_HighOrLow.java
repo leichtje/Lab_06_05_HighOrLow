b4aaa87ef302e031e6d3c773f5333d8cf4d05c32
leichtje <113708612+leichtje@users.noreply.github.com>
2022-10-02 18:13:41 -0400

    Reconfig

--- src/Lab_06_05_HighOrLow.java
+++ src/Lab_06_05_HighOrLow.java
@@ -10,36 +10,40 @@ public class Lab_06_05_HighOrLow {
         int val = generator.nextInt(10) + 1;
 
 
-
         Scanner in = new Scanner(System.in);
 
-        System.out.println("What do you think the number is?");
-            if (in.hasNextInt()){
-                userGuess = in.nextInt();
-                in.nextLine();
+        System.out.println("What do you think the number is?[1-10]");
+        if (in.hasNextInt()) {
+            userGuess = in.nextInt();
+            in.nextLine();
+
+            if (userGuess >= 1 && userGuess <= 10) {
 
                 System.out.println("The random variable is: " + val);
 
-                if (userGuess == val){
+                if (userGuess == val) {
                     System.out.println("Your guess was on the money!!");
-                } else if (userGuess < val && userGuess>=1){
+                } else if (userGuess < val) {
                     System.out.println("Your guess was lower than the number.");
-                } else if (userGuess> val && userGuess<=10) {
+                } else if (userGuess > val) {
                     System.out.println("Your guess was higher than the number.");
-                }else{
+                } else {
                     trash = in.nextLine();
-                    System.out.println("You entered a invalid number: " + trash+ " Please try again.");
+                    System.out.println("You entered a invalid number: " + trash + " Please try again.");
                     System.exit(0);
                 }
-
-
-            }else{
-                trash = in.nextLine();
-                System.out.println("You entered a invalid number: " + trash+ " Please try again.");
+            } else {
+                System.out.println("You entered a invalid number: " + userGuess + " Please try again.");
                 System.exit(0);
             }
 
+        } else {
+            trash = in.nextLine();
+            System.out.println("You entered a invalid number: " + trash + " Please try again.");
+            System.exit(0);
+        }
 
 
     }
+
 }
